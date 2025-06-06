c588db787271c4de7206b517df4721101e8cdf7c
AdityaGhamat <adityaghamat01@gmail.com>
2025-06-06 22:10:34 +0530

    bi-many to many relation between posts and tags.

--- src/tags/entity/tag.entity.ts
+++ src/tags/entity/tag.entity.ts
@@ -1,8 +1,10 @@
+import { Post } from 'src/posts/entity/post.entity';
 import {
   Column,
   CreateDateColumn,
   DeleteDateColumn,
   Entity,
+  ManyToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
@@ -42,6 +44,9 @@ export class Tag {
     nullable: true,
   })
   featuredImageUrl: string;
+
+  @ManyToMany(() => Post, (post) => post.tags)
+  posts: Post[];
 
   @CreateDateColumn()
   createDate: Date;
