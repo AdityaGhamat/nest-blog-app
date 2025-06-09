c52f40d884a8b553857330660d3a55644ff21ddc
AdityaGhamat <adityaghamat01@gmail.com>
2025-06-09 21:48:38 +0530

    added validation on environment file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,10 @@ import { PaginationModule } from './common/pagination/pagination.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import envValidation from './config/env.validation';
+import jwtConfig from './auth/config/jwt.config';
+import { JwtModule } from '@nestjs/jwt';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './auth/guards/access-token/access-token.guard';
 const Env = process.env.NODE_ENV;
 
 @Module({
@@ -40,8 +44,16 @@ const Env = process.env.NODE_ENV;
     PostsModule,
     TagsModule,
     PaginationModule,
+    ConfigModule.forFeature(jwtConfig),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/config/env.validation.ts
+++ src/config/env.validation.ts
@@ -5,4 +5,8 @@ export default Joi.object({
     .valid('production', 'development', 'test', 'staging')
     .default('development'),
   DATABASE_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+  JWT_TOKEN_AUDIENCE: Joi.string().required(),
+  JWT_TOKEN_ISSUER: Joi.string().required(),
+  JWT_ACCESS_TOKEN_TTL: Joi.string().required(),
 });
