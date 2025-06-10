839f92226c04632090ea7ee4544413e773f90688
AdityaGhamat <adityaghamat01@gmail.com>
2025-06-10 21:51:27 +0530

    added endpoint for refreshtokenprovider in auth module.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { SignInDTO } from './dto/signin.dto';
 import { AuthService } from './provider/auth.service';
 import { Auth } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
+import { RefreshTokenDTO } from './dto/refresh-token.dto';
 @Controller('auth')
 export class AuthController {
   constructor(
@@ -17,5 +18,11 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   public async SignIn(@Body() signInDto: SignInDTO) {
     return this.authService.signIn(signInDto);
+  }
+  @Auth(AuthType.None)
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  public async RefreshTokens(@Body() refreshTokenDto: RefreshTokenDTO) {
+    return this.authService.refreshToken(refreshTokenDto);
   }
 }
--- src/auth/provider/auth.service.ts
+++ src/auth/provider/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { SignInDTO } from '../dto/signin.dto';
 
 import { SignInProvider } from './sign-in.provider';
+import { RefreshTokenDTO } from '../dto/refresh-token.dto';
+import { RefreshTokenProvider } from './refresh-token.provider';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +12,15 @@ export class AuthService {
      * Injecting signIn provider
      */
     private readonly signinProvider: SignInProvider,
+    /**
+     * Injecting Refresh Token Provider
+     */
+    private readonly refreshTokenProvider: RefreshTokenProvider,
   ) {}
   public async signIn(signInDto: SignInDTO) {
     return this.signinProvider.signIn(signInDto);
+  }
+  public async refreshToken(refreshTokenDto: RefreshTokenDTO) {
+    return this.refreshTokenProvider.refreshTokens(refreshTokenDto);
   }
 }
