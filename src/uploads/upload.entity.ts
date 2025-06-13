c923af58e0069ec37cee9d8577cbda176fb38bc9
AdityaGhamat <adityaghamat01@gmail.com>
2025-06-13 21:08:38 +0530

    setup of upload module and entity

---
+++ src/uploads/upload.entity.ts
@@ -0,0 +1,56 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { fileTypes } from './enums/filetypes.enum';
+
+@Entity()
+export class Upload {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 1024,
+  })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 1024,
+  })
+  path: string;
+
+  @Column({
+    type: 'enum',
+    enum: fileTypes,
+    default: fileTypes.IMAGE,
+    nullable: false,
+  })
+  type: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: false,
+    length: 128,
+  })
+  mime: string;
+
+  @Column({
+    type: 'int8',
+    nullable: false,
+    length: 1024,
+  })
+  size: number;
+
+  @CreateDateColumn()
+  createDate: Date;
+
+  @UpdateDateColumn()
+  updateDate: Date;
+}
